37080de3551f98ff37b9a57a17d15a4ae3dee162
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-16 19:26:29 +0900

    feat/#4: chat 아이콘 컴포넌트 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,4 @@
 export {default as NotificationIcon} from './NotificationIcon';
 export {default as SignoutIcon} from './SignoutIcon';
 export {default as UserIcon} from './UserIcon';
+export {default as ChatIcon} from './ChatIcon';
