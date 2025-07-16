0eeaf64220bd4ed8472a37595e2d2796206da6b5
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-17 01:43:56 +0900

    chore/#4: SVG 파일 SVGR로 변환하여 React 컴포넌트 적용

--- src/assets/svg/ChatIcon.tsx
+++ src/assets/svg/ChatIcon.tsx
@@ -3,17 +3,15 @@ import type {SVGProps} from 'react';
 const SvgChatIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    height='63'
-    viewBox='0 0 63 63'
     fill='none'
+    viewBox='0 0 28 27'
     {...props}>
     <path
-      fill='#fff'
-      d='M31.7523 42.3877C34.4148 42.3873 37.0016 41.5014 39.1054 39.8695C41.2093 38.2376 42.7106 35.9523 43.3731 33.3735C44.0356 30.7947 43.8216 28.0688 42.7648 25.625C41.7081 23.1811 39.8686 21.1581 37.5359 19.8744C35.2032 18.5908 32.5099 18.1194 29.8799 18.5344C27.2499 18.9495 24.8326 20.2274 23.0086 22.167C21.1846 24.1067 20.0575 26.5979 19.8047 29.2484C19.5519 31.8989 20.1878 34.5582 21.6123 36.8077L19.7523 42.3877L25.3323 40.5277C27.2517 41.7459 29.4789 42.3912 31.7523 42.3877Z'
-      stroke='white'
-      stroke-width='2.4'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      stroke='#fff'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2.4}
+      d='M14.108 25.389a12 12 0 1 0-10.14-5.58l-1.86 5.58 5.58-1.86a11.95 11.95 0 0 0 6.42 1.86'
     />
   </svg>
 );
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,4 @@
+export {default as ChatIcon} from './ChatIcon';
 export {default as NotificationIcon} from './NotificationIcon';
 export {default as SignoutIcon} from './SignoutIcon';
 export {default as UserIcon} from './UserIcon';
-export {default as ChatIcon} from './ChatIcon';
