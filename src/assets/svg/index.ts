412efebbbaef5be07e0a9bb06ff9e17ba5f6548c
suminb99 <suminbaek99@gmail.com>
2025-07-17 19:52:58 +0900

    #3 feat: ellipsis 아이콘 컴포넌트 export

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,4 @@
+export {default as EllipsisIcon} from './EllipsisIcon';
 export {default as NotificationIcon} from './NotificationIcon';
 export {default as SignoutIcon} from './SignoutIcon';
 export {default as UserIcon} from './UserIcon';
