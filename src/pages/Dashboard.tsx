b877c365de0204086f73854c07da214fb718523c
suminb99 <suminbaek99@gmail.com>
2025-07-17 19:51:09 +0900

    #3 feat: dashboard page 컴포넌트 추가

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,20 @@
+import CourseList from '../components/Dashboard/CourseList';
+import ScheduleList from '../components/Dashboard/ScheduleList';
+import StudentHeader from '../components/Header/StudentHeader';
+
+const Dashboard = () => {
+  return (
+    <div className='min-h-screen h-full py-10 bg-background flex-col'>
+      <StudentHeader />
+      <div className='w-289 mx-auto my-0'>
+        <h1 className='text-[28px] pt-10 pb-4'>내 대시보드</h1>
+        <div className='flex flex-row justify-between'>
+          <CourseList />
+          <ScheduleList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
