d58279b5c4a0f5a445539a1a369d9dda48bc5474
suminb99 <suminbaek99@gmail.com>
2025-07-18 18:09:39 +0900

    #3 feat: 학생 페이지route 추가 - 기본 페이지로 대시보드 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         {/* 학생 전용 영역 */}
         <Route path='/student' element={<Layout />}>
           {/* 추가 페이지들 */}
+          <Route index element={<Dashboard />} />
         </Route>
 
         {/* 관리자 전용 영역 */}
