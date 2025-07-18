2e4bbb17c97e87b7e8574a646acf109ef613be57
suminb99 <suminbaek99@gmail.com>
2025-07-19 00:34:23 +0900

    #3 feat: 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/common/Dashboard';
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
         {/* 관리자 전용 영역 */}
         <Route path='/admin' element={<Layout />}>
           {/* 추가 페이지들 */}
+          <Route index element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
