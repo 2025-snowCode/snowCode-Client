ffe4ca561e21b3d060af1a6935d9ed8ccd19c671
suminb99 <suminbaek99@gmail.com>
2025-07-20 03:24:09 +0900

    #3 feat: 대시보드 영역 width 및 패딩값 수정

--- src/pages/common/Dashboard.tsx
+++ src/pages/common/Dashboard.tsx
@@ -3,13 +3,11 @@ import ScheduleList from '../../components/common/Dashboard/ScheduleList';
 
 const Dashboard = () => {
   return (
-    <div className='min-h-screen h-full py-10 bg-background flex-col'>
-      <div className='w-289 mx-auto my-0'>
-        <h1 className='text-[28px] pt-10 pb-4'>내 대시보드</h1>
-        <div className='flex flex-row justify-between'>
-          <CourseList />
-          <ScheduleList />
-        </div>
+    <div className='w-full'>
+      <h1 className='text-[28px] px-1 pb-4 font-medium'>내 대시보드</h1>
+      <div className='flex justify-between'>
+        <CourseList />
+        <ScheduleList />
       </div>
     </div>
   );
