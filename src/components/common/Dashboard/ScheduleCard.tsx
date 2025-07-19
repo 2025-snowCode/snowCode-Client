caf710114f92effcc8f256f7759ee95cf82671c8
suminb99 <suminbaek99@gmail.com>
2025-07-19 16:44:10 +0900

    #3 style: ScheduleCard 텍스트 영역에 width 설정 및 truncate 적용

--- src/components/common/Dashboard/ScheduleCard.tsx
+++ src/components/common/Dashboard/ScheduleCard.tsx
@@ -6,10 +6,10 @@ interface Props extends Assignment {
 const ScheduleCard = (props: Props) => {
   return (
     <div className='flex items-start p-7 gap-6 border-0 bg-white w-[396px] mb-5 rounded-3xl'>
-      <div className='border-0 text-white bg-radial-[at_38%_100%] from-[#7D63FF] to-[#AB9AFF] px-4 py-1 rounded-full whitespace-nowrap'>
+      <div className='border-0 text-white px-4 py-1 rounded-full whitespace-nowrap bg-radial-[at_38%_100%] from-[#7D63FF] to-[#AB9AFF]'>
         {`${props.remainingDays}일 전`}
       </div>
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-62'>
         <p className='text-lg'>{`${props.course} (${props.section})`}</p>
         <p className='text-base truncate font-light text-light-black'>
           {props.assignment}
