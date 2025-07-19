c0afbaebd9c280f9bb7ae289b5f1e7e2ca6ee1d2
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-19 22:38:06 +0900

    #5 refactor: 랜딩 페이지 구조 개편 및 상태 관리 도입

--- src/pages/common/LandingPage.tsx
+++ src/pages/common/LandingPage.tsx
@@ -1,8 +1,13 @@
+import {useState} from 'react';
 import snowCodeEntry from '/src/assets/images/snowCode_entry.svg';
 import googleLogo from '/src/assets/images/google_logo.svg';
 import ActionButton from '../../components/common/ActionButton';
 
 export default function LandingPage() {
+  const [selected, setSelected] = useState<'none' | 'student' | 'admin'>(
+    'none'
+  );
+
   return (
     <div className='flex flex-col justify-center items-center min-h-[calc(100vh-120px)] text-center'>
       <img src={snowCodeEntry} alt='로고' className='w-[433px] h-[433px]' />
@@ -25,7 +30,7 @@ export default function LandingPage() {
           className='flex gap-2 justify-center text-black-primary text-lg font-normal leading-[150%] underline decoration-solid'
           aria-label='구글 이메일 로그인'>
           <img src={googleLogo} alt='로고' className='w-[26px] h-[26px]' />
-          구글 이메일으로 로그인
+          구글 이메일로 로그인
         </button>
       </div>
     </div>
