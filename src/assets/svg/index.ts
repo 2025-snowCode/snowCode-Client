03e2a711219b05111fd5d0878b55c943ef682197
suminb99 <suminbaek99@gmail.com>
2025-07-20 03:25:53 +0900

    #3 feat: add 아이콘 추가 및 svgr로 컴포넌트화

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,6 +1,7 @@
+export {default as AddIcon} from './AddIcon';
 export {default as ArrowIcon} from './ArrowIcon';
+export {default as ChatIcon} from './ChatIcon';
+export {default as EllipsisIcon} from './EllipsisIcon';
 export {default as NotificationIcon} from './NotificationIcon';
 export {default as SignoutIcon} from './SignoutIcon';
 export {default as UserIcon} from './UserIcon';
-export {default as ChatIcon} from './ChatIcon';
-export {default as EllipsisIcon} from './EllipsisIcon';
