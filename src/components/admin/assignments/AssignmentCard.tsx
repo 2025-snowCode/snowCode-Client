a426e7cc1166d7da1e4605258e0c24f06892dd0c
suminb99 <suminbaek99@gmail.com>
2025-07-20 02:49:15 +0900

    #9 feat: 문제 카드 UI

---
+++ src/components/admin/assignments/AssignmentCard.tsx
@@ -0,0 +1,21 @@
+import type {Assignment} from '../../common/Dashboard/types';
+import {DragAndDropIcon, DeleteIcon, EditIcon} from '../../../assets/svg';
+
+interface Props extends Assignment {}
+
+const AssignmentCard = (props: Props) => {
+  return (
+    <div className='bg-background w-[612px] py-4 rounded-[9px] my-1 flex items-center justify-between px-[21.5px]'>
+      <div className='flex-center gap-4'>
+        <DragAndDropIcon width={8} height={13} />
+        <div className=''>{props.assignment}</div>
+      </div>
+      <div className='flex-center gap-6'>
+        <EditIcon width={14} height={16} />
+        <DeleteIcon width={12} height={12} />
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentCard;
