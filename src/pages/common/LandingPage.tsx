7423c7b77500e456b9ffe66c808878cc76cf6ce7
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-19 22:50:37 +0900

    #5 feat: 사용자 유형 선택 기능 및 이미지 변경

--- src/pages/common/LandingPage.tsx
+++ src/pages/common/LandingPage.tsx
@@ -1,16 +1,37 @@
 import {useState} from 'react';
 import snowCodeEntry from '/src/assets/images/snowCode_entry.svg';
+import snowCodeStudent from '/src/assets/images/snowCode_student.svg';
+import snowCodeAdmin from '/src/assets/images/snowCode_admin.svg';
 import googleLogo from '/src/assets/images/google_logo.svg';
 import ActionButton from '../../components/common/ActionButton';
 
+type HoverState = 'none' | 'student' | 'admin';
+
 export default function LandingPage() {
+  const [hover, setHover] = useState<HoverState>('none');
   const [selected, setSelected] = useState<'none' | 'student' | 'admin'>(
     'none'
   );
 
+  const imgSrc =
+    hover === 'student'
+      ? snowCodeStudent
+      : hover === 'admin'
+      ? snowCodeAdmin
+      : snowCodeEntry;
+
   return (
     <div className='flex flex-col justify-center items-center min-h-[calc(100vh-120px)] text-center'>
-      <img src={snowCodeEntry} alt='로고' className='w-[433px] h-[433px]' />
+      <div
+        className={`relative w-[433px] ${
+          imgSrc === snowCodeStudent ? 'h-[439px]' : 'h-[433px]'
+        }`}>
+        <img
+          src={imgSrc}
+          alt='로고'
+          className='w-full h-full transition-all duration-300'
+        />
+      </div>
 
       <div className='flex flex-col gap-[60px] -mt-12'>
         <div className='flex flex-col gap-7'>
@@ -21,8 +42,20 @@ export default function LandingPage() {
           </span>
 
           <div className='flex gap-11 justify-center'>
-            <ActionButton label='학생' />
-            <ActionButton label='관리자' />
+            <ActionButton
+              label='학생'
+              onClick={() => setSelected('student')}
+              onMouseEnter={() => setHover('student')}
+              onMouseLeave={() => selected === 'none' && setHover('none')}
+              selected={selected === 'student'}
+            />
+            <ActionButton
+              label='관리자'
+              onClick={() => setSelected('admin')}
+              onMouseEnter={() => setHover('admin')}
+              onMouseLeave={() => selected === 'none' && setHover('none')}
+              selected={selected === 'admin'}
+            />
           </div>
         </div>
 
