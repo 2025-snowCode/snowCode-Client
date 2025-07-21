98c1162af79b3464dd201737507281cc5e36b8db
suminb99 <suminbaek99@gmail.com>
2025-07-21 13:11:39 +0900

    #9 feat: 강의 선택 드롭다운 UI, 간단한 open-close 상태관리 도입

--- src/components/admin/assignments/CourseSelector.tsx
+++ src/components/admin/assignments/CourseSelector.tsx
@@ -1,5 +1,75 @@
+import {useState} from 'react';
+import type {Assignment} from '../../common/Dashboard/types';
+import {ArrowdownIcon} from '../../../assets/svg';
+const assignments: Assignment[] = [
+  {
+    id: 1,
+    course: '소프트웨어의 이해',
+    section: '005',
+    assignment: '음수 구별하기',
+  },
+  {
+    id: 2,
+    course: '데이터 구조와 알고리즘',
+    section: '001',
+    assignment: '스택 (Stack) 구현하기',
+  },
+  {
+    id: 3,
+    course: '데이터 구조와 알고리즘',
+    section: '001',
+    assignment: '스택 (Stack) 구현하기',
+  },
+  {
+    id: 4,
+    course: '데이터 구조와 알고리즘',
+    section: '001',
+    assignment: '스택 (Stack) 구현하기',
+  },
+  {
+    id: 5,
+    course: '데이터 구조와 알고리즘',
+    section: '001',
+    assignment: '스택 (Stack) 구현하기',
+  },
+  {
+    id: 6,
+    course: '데이터 구조와 알고리즘',
+    section: '001',
+    assignment: '스택 (Stack) 구현하기',
+  },
+];
+
 const CourseSelector = () => {
-  return <></>;
+  const [open, setOpen] = useState(false);
+
+  const handleOnClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className='space-y-2 relative pt-7'>
+      <div
+        className='flex items-center w-[324px] h-[45px] border-1 rounded-[9px] border-stroke gap-40 px-[14px]'
+        onClick={handleOnClick}>
+        <span className='text-light-black'>강의를 선택하세요</span>
+        <ArrowdownIcon width={13.5} height={8} />
+      </div>
+      {open && (
+        <ul className='flex flex-col w-[324px] bg-gray border-0 rounded-[9px] absolute shadow-dropdown'>
+          {assignments.map((a, index) => (
+            <li
+              key={a.id}
+              className={`border-stroke px-[15.5px] py-[13px] ${
+                index !== assignments.length - 1 ? 'border-b-1' : ''
+              }`}>
+              {a.course} {a.section}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default CourseSelector;
