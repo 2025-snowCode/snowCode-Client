b61316e7612962806ee02edcaf3cbc840f3be144
suminb99 <suminbaek99@gmail.com>
2025-07-21 13:09:14 +0900

    #9 refactor: CourseSelector wrapper 제거

--- src/pages/admin/ManageAssignments.tsx
+++ src/pages/admin/ManageAssignments.tsx
@@ -46,9 +46,7 @@ const ManageAssignments = () => {
     <div className='w-[900px] h-[760px] bg-white rounded-[30px]'>
       <div className='flex flex-col h-1/4 px-14 justify-center border-b-1 border-stroke'>
         <h3 className='text-2xl font-semibold'>문제 관리</h3>
-        <div>
-          <CourseSelector />
-        </div>
+        <CourseSelector />
       </div>
       <div className='px-14'>
         <p className='mb-3 mt-10 font-medium text-lg'>
