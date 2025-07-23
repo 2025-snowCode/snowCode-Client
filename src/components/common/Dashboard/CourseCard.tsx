dde745a7a67e4681b53a917d7e4450719cc78044
suminb99 <suminbaek99@gmail.com>
2025-07-23 19:30:22 +0900

    #3 refactor: CourseCard props 구조 분해 할당 적용

--- src/components/common/Dashboard/CourseCard.tsx
+++ src/components/common/Dashboard/CourseCard.tsx
@@ -1,7 +1,7 @@
 import {EllipsisIcon} from '../../../assets/svg';
-import type {Course} from './types';
+import type {Course, UserType} from './types';
 
-interface Props extends Course {}
+interface CourseCardProps extends Course {}
 
 const getSemesterNumber = (semester: string): string => {
   switch (semester) {
@@ -18,19 +18,25 @@ const getSemesterNumber = (semester: string): string => {
   }
 };
 
-const CourseCard = (props: Props) => {
-  props.semester;
+const CourseCard = ({
+  semester,
+  year,
+  section,
+  title,
+  description,
+  unitCount,
+  assignmentCount,
+}: CourseCardProps) => {
+  semester;
   return (
     <div className='min-h-36 rounded-3xl bg-gray border-0 flex-center my-4 shadow-card'>
       <section className='p-8 bg-white rounded-tl-3xl rounded-bl-3xl'>
         <p className='text-sm font-light text-light-black'>
-          {`${props.year}\\${getSemesterNumber(props.semester)}학기\\${
-            props.section
-          }분반`}
+          {`${year}\\${getSemesterNumber(semester)}학기\\${section}분반`}
         </p>
-        <h3 className='text-[22px]'>{props.title}</h3>
+        <h3 className='text-[22px]'>{title}</h3>
         <p className='text-base font-light text-secondary-black'>
-          {props.description}
+          {description}
         </p>
       </section>
       <section className='relative'>
@@ -40,11 +46,11 @@ const CourseCard = (props: Props) => {
         <div className='flex-center p-4'>
           <div className='flex-center flex-col whitespace-nowrap border-r border-[#7A768C] px-4'>
             <div>단원 수</div>
-            <div>{`${props.unitCount}개`}</div>
+            <div>{`${unitCount}개`}</div>
           </div>
           <div className='flex-center flex-col whitespace-nowrap px-4'>
             <div>문제 수</div>
-            <div>{`${props.assignmentCount}개`}</div>
+            <div>{`${assignmentCount}개`}</div>
           </div>
         </div>
       </section>
