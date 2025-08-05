89fdf1369077ddd896036f87496e3811fe83050f
suminb99 <suminbaek99@gmail.com>
2025-08-05 23:37:27 +0900

    #9 refactor: ManageAssignment에서 공통 데이터 전달 방식으로 구조 리펙토링

--- src/components/admin/assignments/AssignmentCard.tsx
+++ src/components/admin/assignments/AssignmentCard.tsx
@@ -1,14 +1,14 @@
-import type {Assignment} from '../../common/Dashboard/types';
 import {DragAndDropIcon, DeleteIcon, EditIcon} from '../../../assets/svg';
+import type {Assignment} from './dummy/types';
 
-interface Props extends Assignment {}
+interface AssignmentCardProps extends Assignment {}
 
-const AssignmentCard = (props: Props) => {
+const AssignmentCard = ({title}: AssignmentCardProps) => {
   return (
     <div className='bg-background w-[612px] py-4 rounded-[9px] my-1 flex items-center justify-between px-[21.5px]'>
       <div className='flex-center gap-4'>
         <DragAndDropIcon width={8} height={13} />
-        <div className=''>{props.assignment}</div>
+        <div>{title}</div>
       </div>
       <div className='flex-center gap-6'>
         <EditIcon width={14} height={16} />
--- src/components/admin/assignments/AssignmentList.tsx
+++ src/components/admin/assignments/AssignmentList.tsx
@@ -1,11 +1,30 @@
-const AssignmentList = () => {
+import type {Course} from './dummy/types';
+import AssignmentCard from './AssignmentCard';
+
+interface AssignmentListProps {
+  courses: Course[];
+}
+
+const AssignmentList = ({courses}: AssignmentListProps) => {
+  // 문제 수 세기
+  const countAssignments = () => {
+    let count = 0;
+    courses.forEach((course) => {
+      count += course.assignments.length;
+    });
+
+    return count;
+  };
+
   return (
     <div>
-      <p className='mb-3 mt-10 font-medium text-lg'>{assignments.length}문제</p>
+      <p className='mb-3 mt-10 font-medium text-lg'>{countAssignments()}문제</p>
       <div className='flex flex-col'>
-        {assignments.map((assignment) => (
-          <AssignmentCard key={assignment.id} {...assignment} />
-        ))}
+        {courses.map((course) =>
+          course.assignments.map((assignment) => (
+            <AssignmentCard key={assignment.id} {...assignment} />
+          ))
+        )}
       </div>
     </div>
   );
