845b96599328904b300272fbb1a82ac13ebb3bb4
suminb99 <suminbaek99@gmail.com>
2025-08-06 03:32:29 +0900

    #9 refactor: AssignmentCard 컴포넌트 select prop 추가 및 스타일 분기

--- src/components/admin/assignments/AssignmentCard.tsx
+++ src/components/admin/assignments/AssignmentCard.tsx
@@ -1,19 +1,54 @@
-import {DragAndDropIcon, DeleteIcon, EditIcon} from '../../../assets/svg';
+import {useState} from 'react';
+import {
+  SingleEllipsisIcon,
+  DragAndDropIcon,
+  DeleteIcon,
+  EditIcon,
+} from '../../../assets/svg';
 import type {Assignment} from './dummy/types';
 
-interface AssignmentCardProps extends Assignment {}
+interface AssignmentCardProps extends Assignment {
+  select?: boolean;
+  onLinkAssignments?: (id: number, title: string, isSelected: boolean) => void;
+}
 
-const AssignmentCard = ({title}: AssignmentCardProps) => {
+const AssignmentCard = ({
+  id,
+  title,
+  select,
+  onLinkAssignments,
+}: AssignmentCardProps) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const handleOnClick = () => {
+    const selectedState = !isSelected;
+    setIsSelected(selectedState);
+    onLinkAssignments?.(id, title, selectedState);
+  };
+
   return (
-    <div className='bg-background w-[612px] py-4 rounded-[9px] my-1 flex items-center justify-between px-[21.5px]'>
-      <div className='flex-center gap-4'>
-        <DragAndDropIcon width={8} height={13} />
+    <div
+      onClick={handleOnClick}
+      className={`bg-background w-[612px] py-4 rounded-[9px] my-1 flex items-center justify-between px-[21.5px] ${
+        isSelected ? 'border-1 border-primary' : 'border-1 border-background'
+      }`}>
+      <div className='flex-center gap-4 cursor-pointer '>
+        {select ? (
+          <SingleEllipsisIcon
+            width={12}
+            height={12}
+            stroke={isSelected ? '#856cff' : '#ffffff'}
+          />
+        ) : (
+          <DragAndDropIcon width={8} height={13} />
+        )}
         <div>{title}</div>
       </div>
-      <div className='flex-center gap-6 cursor-pointer'>
-        <EditIcon width={14} height={16} />
-        <DeleteIcon width={12} height={12} />
-      </div>
+      {!select && (
+        <div className='flex-center gap-6 cursor-pointer'>
+          <EditIcon width={14} height={16} />
+          <DeleteIcon width={12} height={12} />
+        </div>
+      )}
     </div>
   );
 };
