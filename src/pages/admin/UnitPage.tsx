20da864dc7f5284ab7765469371ba0a40a573029
suminb99 <suminbaek99@gmail.com>
2025-08-18 11:57:56 +0900

    #15 feat: 단원 개설 페이지 컴포넌트 추가

---
+++ src/pages/admin/UnitPage.tsx
@@ -0,0 +1,11 @@
+import UnitLayout from '../../components/admin/units/UnitLayout';
+
+const UnitPage = () => {
+  return (
+    <div>
+      <UnitLayout />
+    </div>
+  );
+};
+
+export default UnitPage;
