71d7c72777a5c3203b2c460729727c29af74cb62
suminb99 <suminbaek99@gmail.com>
2025-08-18 12:54:24 +0900

    #15 style: 레이아웃 높이 설정 및 overflow 시 scroll 적용

--- src/components/admin/units/UnitEditor.tsx
+++ src/components/admin/units/UnitEditor.tsx
@@ -6,9 +6,9 @@ import {unit} from './dummy/unit';
 const UnitEditor = () => {
   const unitInfo = unit.response;
   return (
-    <div className='bg-background w-[670px] h-[629px] rounded-[30px]'>
+    <div className='bg-background w-[670px] h-[629px] rounded-[30px] overflow-y-scroll'>
       <div className='bg-[#EDE9FF] px-[30.1px] py-[16.5px] rounded-t-[30px]'>
-        <h3>1. 단원</h3>
+        <h3 className='text-lg font-medium'>1. 단원</h3>
       </div>
       <UnitForm {...unitInfo} />
     </div>
--- src/components/admin/units/UnitLayout.tsx
+++ src/components/admin/units/UnitLayout.tsx
@@ -4,10 +4,10 @@ import UnitList from './UnitList';
 const UnitLayout = () => {
   return (
     <div className='flex gap-[14.83px]'>
-      <aside className='w-112.5 bg-white rounded-[30px]'>
+      <aside className='w-112.5 h-188 overflow-y-scroll bg-white rounded-[30px]'>
         <UnitList />
       </aside>
-      <section className='w-[737.3px] bg-white rounded-[30px] pl-[30.6px] pr-[36.7px] pt-[33.72px] pb-[89.2px]'>
+      <section className='w-[737.3px] h-188 bg-white rounded-[30px] pl-[30.6px] pr-[36.7px] pt-[33.72px] pb-[89.2px]'>
         <UnitEditor />
         <div className='flex justify-end gap-[21.8px] pt-[23.4px]'>
           <Button theme='primaryWhite' text='취소' />
