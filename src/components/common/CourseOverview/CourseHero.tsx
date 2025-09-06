cc4d67a776f7a09c0105e80dff6c70f7e4f387af
suminb99 <suminbaek99@gmail.com>
2025-09-06 12:53:19 +0900

    #16 feat: 강의 상세 페이지 Hero 섹션 컴포넌트 추가

---
+++ src/components/common/CourseOverview/CourseHero.tsx
@@ -0,0 +1,57 @@
+import {useLocation} from 'react-router-dom';
+import snowcodeOverviewMini from '../../../assets/images/snowcode_overview_mini.svg';
+import {getSemesterLabel} from '../../../utils/getSemesterLabel';
+import type {SemesterCode} from '../../admin/assignments/dummy/types';
+import CourseActionsBar from './CourseActionsBar';
+import CourseStat from './CourseStat';
+
+interface CoursHeroProps {
+  title: string;
+  year: number;
+  semester: SemesterCode;
+  section: string;
+  unitCount: number;
+  assignmentCount: number;
+  studentCount?: number;
+  isActiveCourse: boolean;
+}
+
+const CourseHero = ({
+  title,
+  year,
+  semester,
+  section,
+  unitCount,
+  assignmentCount,
+  studentCount,
+  isActiveCourse,
+}: CoursHeroProps) => {
+  const pathname = useLocation().pathname;
+  const isAdmin = pathname.startsWith('/admin');
+
+  return (
+    <div className='w-full h-[280px] flex-center flex-col rounded-t-[30px] bg-radial-[80%_74%_at_34%_0%] from-[#AC9BFF] from-0% to-[#7D63FF] to-100%'>
+      {isAdmin && isActiveCourse && (
+        <div className='pl-190'>
+          <CourseActionsBar isActive={isActiveCourse} />{' '}
+        </div>
+      )}
+      <img className='pb-[4px]' src={snowcodeOverviewMini} alt='logo' />
+      <span className='pb-[1px] text-2xl text-white font-medium leading-9'>
+        {title}
+      </span>
+      <span className='pb-[11px] text-base text-white font-normal leading-6'>{`${year}년 ${getSemesterLabel(
+        semester
+      )}학기 ${section}분반`}</span>
+      {isActiveCourse && (
+        <CourseStat
+          unitCount={unitCount}
+          assignmentCount={assignmentCount}
+          studentCount={studentCount}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CourseHero;
