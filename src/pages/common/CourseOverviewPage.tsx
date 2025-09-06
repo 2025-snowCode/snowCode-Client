5116c94ecf3acd9d1a2f494bcd0c5d410c254357
suminb99 <suminbaek99@gmail.com>
2025-09-06 12:50:33 +0900

    #16 feat: CourseOverviewPage 전체 레이아웃 및 주요 컴포넌트 적용

---
+++ src/pages/common/CourseOverviewPage.tsx
@@ -0,0 +1,39 @@
+import CourseContent from '../../components/common/CourseOverview/CourseContent';
+import CourseHero from '../../components/common/CourseOverview/CourseHero';
+import {courseResponse} from '../../components/common/CourseOverview/dummy/response';
+import EmptyCourse from '../../components/common/CourseOverview/EmptyCourse';
+
+function countAssignments() {
+  let assignmentCount = 0;
+  courseResponse.response.units.forEach(
+    (unit) => (assignmentCount += unit.assignmentCount)
+  );
+
+  return assignmentCount;
+}
+
+const isActiveCourse = courseResponse.response.unitCount !== 0;
+
+const CourseOverviewPage = () => {
+  return (
+    <div className='w-full min-h-screen flex flex-col'>
+      <CourseHero
+        title={courseResponse.response.title}
+        year={courseResponse.response.year}
+        semester={courseResponse.response.semester}
+        section={courseResponse.response.section}
+        unitCount={courseResponse.response.unitCount}
+        assignmentCount={countAssignments()}
+        studentCount={courseResponse.response.studentCount}
+        isActiveCourse={isActiveCourse}
+      />
+      {isActiveCourse ? (
+        <CourseContent units={courseResponse.response.units} />
+      ) : (
+        <EmptyCourse />
+      )}
+    </div>
+  );
+};
+
+export default CourseOverviewPage;
