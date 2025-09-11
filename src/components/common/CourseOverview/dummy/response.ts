34965a0c92d874b53ccf93ffe1eef59b9e35aa40
suminb99 <suminbaek99@gmail.com>
2025-09-12 00:21:38 +0900

    #16 feat: 관리자용 더미 추가

--- src/components/common/CourseOverview/dummy/response.ts
+++ src/components/common/CourseOverview/dummy/response.ts
@@ -64,3 +64,52 @@ export const courseResponse: CourseResponse = {
     ],
   },
 };
+
+// export const courseResponse: CourseResponse = {
+//   success: true,
+//   response: {
+//     id: 1,
+//     title: '소프트웨어의 이해',
+//     year: 2025,
+//     semester: 'FIRST',
+//     section: '005',
+//     studentCount: 56,
+//     unitCount: 2,
+//     units: [
+//       {
+//         id: 1,
+//         title: '변수와 수식',
+//         releaseDate: '2025-06-19',
+//         dueDate: '2025-06-25',
+//         assignmentCount: 2,
+//         assignments: [
+//           {
+//             id: 1,
+//             title: '음수 구별하기',
+//           },
+//           {
+//             id: 3,
+//             title: '변수 선언 후 값 출력해보기',
+//           },
+//         ],
+//       },
+//       {
+//         id: 3,
+//         title: '반복문과 조건문',
+//         releaseDate: '2025-06-19',
+//         dueDate: '2025-06-25',
+//         assignmentCount: 2,
+//         assignments: [
+//           {
+//             id: 4,
+//             title: '짝수와 음수 구별하기',
+//           },
+//           {
+//             id: 5,
+//             title: '숫자 맞히기 프로그램 만들기 실습',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// };
