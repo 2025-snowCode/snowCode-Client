6e703f21ca75e728b6b8f2f506658b0150b987c9
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-09-22 13:16:12 +0900

    #13 feat: StudentManagementPage 초기 레이아웃 및 검색 UI 추가

---
+++ src/pages/admin/student/studentManagementPage.tsx
@@ -0,0 +1,30 @@
+import AssignmentFormLayout from '@/components/admin/assignments/AssignmentFormLayout';
+import {Search} from '@/assets/svg';
+
+export default function StudentManagementPage() {
+  const titleExtra = (
+    <div className='flex justify-between items-center flex-1'>
+      <button className=' text-primary px-3 py-[6px] rounded-4xl inline-flex h-8 justify-center items-center border border-primary'>
+        소프트웨어 이해 (005)
+      </button>
+      <div className='relative'>
+        <input
+          type='text'
+          placeholder='학번을 검색하세요'
+          className='border border-gray-300 rounded-lg px-4 py-2 pr-10 w-64'
+        />
+        <Search className='absolute right-3 top-3 w-4 h-4 text-gray-400' />
+      </div>
+    </div>
+  );
+
+  return (
+    <AssignmentFormLayout
+      title='학생 관리'
+      titleExtra={titleExtra}
+      content={<div>학생 관리 기능이 여기에 들어갑니다.</div>}
+      onCancel={() => {}}
+      onConfirm={() => {}}
+    />
+  );
+}
