6aa8fb19eb2906f44643f11e34f136301401b4e5
suminb99 <suminbaek99@gmail.com>
2025-12-30 20:37:37 +0900

    (#21) feat: Button 컴포넌트 props 추가 및 variants 확장

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,14 +1,23 @@
 import {tv, type VariantProps} from 'tailwind-variants/lite';
 
 const button = tv({
-  base: 'flex-center px-3 py-1.5 text-center text-base font-medium whitespace-nowrap rounded-[10px] border',
+  base: 'cursor-pointer flex-center text-center gap-2 text-base font-medium whitespace-nowrap rounded-[10px] border',
   variants: {
     color: {
       primary: 'bg-primary text-white border-primary',
-      secondary: 'bg-white text-primary-black border-purple-stroke',
+      secondary:
+        'bg-white text-primary-black border-purple-stroke hover:bg-hover hover:text-white',
       outlinePurple: 'bg-white text-primary border-primary',
       outlineWhite: 'bg-transparent text-white border-white',
       tonal: 'bg-purple-stroke text-secondary-black border-purple-stroke',
+      ghost: 'bg-transparent text-black border-none',
+      ghostWhite: 'bg-white text-secondary-black border-none',
+    },
+    size: {
+      default: 'w-24 h-10 px-3 py-1.5',
+      compact: 'w-fit leading-5 px-3 py-1.5',
+      wide: 'w-40 py-[15px]',
+      none: 'w-fit p-0',
     },
     isIcon: {
       true: 'rounded-full',
@@ -16,6 +25,8 @@ const button = tv({
   },
   defaultVariants: {
     color: 'primary',
+    size: 'default',
+    disabled: false,
     isIcon: false,
   },
 });
@@ -24,12 +35,23 @@ type ButtonVariants = VariantProps<typeof button>;
 
 interface ButtonProps extends ButtonVariants {
   children: React.ReactNode;
+  className?: string;
+  type?: 'button' | 'submit';
+  disabled?: boolean;
   onClick?: () => void;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 }
 
-const Button = ({children, onClick, ...props}: ButtonProps) => {
+const Button = ({
+  children,
+  onClick,
+  type = 'button',
+  disabled = false,
+  ...props
+}: ButtonProps) => {
   return (
-    <button onClick={onClick} className={button(props)}>
+    <button onClick={onClick} disabled={disabled} className={button(props)}>
       {children}
     </button>
   );
