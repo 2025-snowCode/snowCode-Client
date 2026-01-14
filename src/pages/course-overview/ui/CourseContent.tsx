df7379320480b8634d09c7bc18490cc506cb8743
suminb99 <suminbaek99@gmail.com>
2026-01-15 01:18:42 +0900

    #27 refactor: CourseContent 컴포넌트 개선

--- src/pages/course-overview/ui/CourseContent.tsx
+++ src/pages/course-overview/ui/CourseContent.tsx
@@ -1,18 +1,43 @@
-import type {UnitInfo} from '../models/types';
-import Unit from './Unit';
+import type {CourseOverview} from '@/models/course';
+import snowCodeOverview from '@/assets/images/snowcode_overview.svg';
+import UnitItem from './UnitItem';
+import CourseActionsBar from './CourseActionsBar';
 
 interface CourseContentProps {
-  units: UnitInfo[];
+  units: CourseOverview['units'];
+  isActiveCourse: boolean;
+  isAdmin: boolean;
 }
 
-const CourseContent = ({units}: CourseContentProps) => {
+const CourseContent = ({
+  units,
+  isActiveCourse,
+  isAdmin,
+}: CourseContentProps) => {
+  // 빈 강의
+  if (isActiveCourse === false) {
+    return <EmptyCourse isAdmin={isAdmin} />;
+  }
+
   return (
-    <div className='w-full bg-white flex-1 rounded-b-[30px]'>
+    <section className='bg-white w-full flex-1 rounded-b-[30px]'>
       {units.map((unit, index) => (
-        <Unit key={unit.id} index={index} {...unit} />
+        <UnitItem key={unit.id} index={index} {...unit} />
       ))}
-    </div>
+    </section>
   );
 };
 
+const EmptyCourse = ({isAdmin}: {isAdmin: boolean}) => {
+  return (
+    <section className='flex-1 flex-center flex-col bg-white w-full pt-30 pb-41 rounded-b-[30px]'>
+      <img className='mb-11' src={snowCodeOverview} alt='snowCode 이미지' />
+      <p className='mb-1.5 text-lg leading-7 font-normal'>비어있는 강의</p>
+      <p className='mb-7 text-2xl leading-9 font-medium'>
+        아직 생성된 단원이 없어요
+      </p>
+      {isAdmin && <CourseActionsBar isActiveCourse={false} />}
+    </section>
+  );
+};
 export default CourseContent;
