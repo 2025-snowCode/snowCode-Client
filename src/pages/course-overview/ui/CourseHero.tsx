0b822025d816836fc24a079f7da8db112a01c8d3
suminb99 <suminbaek99@gmail.com>
2026-01-14 18:23:41 +0900

    #27 refactor: CourseHero 컴포넌트 구조 및 스타일 개선

--- src/pages/course-overview/ui/CourseHero.tsx
+++ src/pages/course-overview/ui/CourseHero.tsx
@@ -1,9 +1,8 @@
 import snowcodeOverviewMini from '@/assets/images/snowcode_overview_mini.svg';
 import {useLocation} from 'react-router-dom';
-import CourseActionsBar from './CourseActionsBar';
-import CourseStat from './CourseStats';
 import type {CourseOverview} from '@/models/course';
 import {formatSemester} from '@/utils/course';
+import Button from '@/components/common/Button';
 
 interface CourseHeroProps {
   courseData: Omit<CourseOverview, 'units'>;
@@ -11,7 +10,7 @@ interface CourseHeroProps {
   isActiveCourse: boolean;
 }
 
-const CourseHero = ({
+export const CourseHero = ({
   courseData,
   assignmentCount,
   isActiveCourse,
@@ -22,31 +21,66 @@ const CourseHero = ({
   const {title, year, semester, section, unitCount, studentCount} = courseData;
 
   return (
-    <div className='w-full h-[353px] flex-center flex-col rounded-t-[30px] bg-radial-[80%_74%_at_34%_0%] from-[#AC9BFF] from-0% to-[#7D63FF] to-100%'>
+    <section className='w-full flex-center flex-col pt-38.5 pb-12.5 rounded-t-[30px] bg-radial-gradient'>
       {isAdmin && isActiveCourse && (
-        <div className='pl-190 absolute top-36 right-26'>
-          <CourseActionsBar
-            isActive={isActiveCourse}
-            title={title}
-            section={section}
-          />
-        </div>
+        <nav className='pl-190 absolute top-36 right-26'>
+          <CourseActionsBar />
+        </nav>
       )}
-      <img className='pt-22 pb-[4px]' src={snowcodeOverviewMini} alt='logo' />
-      <span className='pb-[1px] text-2xl text-white font-medium leading-9'>
-        {title}
-      </span>
-      <span className='pb-[11px] text-base text-white font-normal leading-6'>{`${year}년 ${formatSemester(
-        semester
-      )}학기 ${section}분반`}</span>
+
+      <CourseInfo
+        title={title}
+        year={year}
+        semester={semester}
+        section={section}
+      />
+
       {isActiveCourse && (
-        <CourseStat
+        <CourseStats
           unitCount={unitCount}
           assignmentCount={assignmentCount}
           studentCount={studentCount}
+          isAdmin={isAdmin}
         />
       )}
-    </div>
+    </section>
+  );
+};
+
+const CourseInfo = ({title, year, semester, section}: any) => {
+  const courseInfo = `${year}년 ${formatSemester(semester)} ${section}분반`;
+
+  return (
+    <article className='flex-center flex-col text-white'>
+      <img src={snowcodeOverviewMini} alt='logo' />
+      <h1 className='pb-[1px] text-2xl font-medium leading-9'>{title}</h1>
+      <p className='pb-[11px] text-base font-normal leading-6'>{courseInfo}</p>
+    </article>
+  );
+};
+
+const CourseStats = ({
+  unitCount,
+  assignmentCount,
+  studentCount,
+  isAdmin,
+}: any) => {
+  const studentInfo = isAdmin && studentCount ? ` | ${studentCount}명` : '';
+  const courseStats = `${unitCount}단원 | ${assignmentCount}문제${studentInfo}`;
+
+  return (
+    <article className='px-3.5 py-1.5 text-center bg-white rounded-[35px]'>
+      <span className='text-base font-normal'>{courseStats}</span>
+    </article>
+  );
+};
+
+const CourseActionsBar = () => {
+  return (
+    <article className='flex gap-5'>
+      <Button color='outlineWhite'>학생 목록</Button>
+      <Button color='outlinePurple'>단원 추가</Button>
+    </article>
   );
 };
 
