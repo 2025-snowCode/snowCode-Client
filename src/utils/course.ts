2f11bb8380ec2da33367a99b5ed56c23a4973911
suminb99 <suminbaek99@gmail.com>
2026-01-19 19:25:43 +0900

    #29 feat: 유틸 함수 추가

--- src/utils/course.ts
+++ src/utils/course.ts
@@ -16,6 +16,22 @@ export const formatDate = (date: string) => {
   return date.replaceAll('-', '.');
 };
 
+// MM.DD 형식으로 변환
+export const formatDateMonthDay = (date: string) => {
+  const [, month, day] = date.split('-');
+
+  return `${month}.${day}`;
+};
+
+// 년/학기/분반 형식으로 변환
+export const formateCourseTermWithSlash = (
+  year: number,
+  semester: SemesterCode,
+  section: string
+): string => {
+  return `${year}\/${formatSemester(semester)}\/${section}분반`;
+};
+
 export const getTotalAssignmentCount = (units: Unit[]): number => {
   return units.reduce((acc, unit) => acc + unit.assignmentCount, 0);
 };
