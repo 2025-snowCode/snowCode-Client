5856cf0ab9059b68c4d571d0b563dd0350e0425b
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-01-20 20:57:39 +0900

    #25 refactor: AssignmentPageLayout에 SurfaceCard 적용

--- src/components/admin/assignments/AssignmentPageLayout.tsx
+++ src/components/admin/assignments/AssignmentPageLayout.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/common/Button';
 import type {Course} from './dummy/types';
 import AddIcon from '@/assets/svg/addIcon.svg?react';
 import {useState} from 'react';
+import SurfaceCard from '@/components/common/SurfaceCard';
 
 interface AssignmentPageLayoutProps {
   title: string;
@@ -24,25 +25,30 @@ const AssignmentPageLayout = ({
   };
 
   return (
-    <div className='mx-auto pt-9 pb-8 w-[900px] bg-white rounded-[30px]'>
-      <div className='flex flex-col h-1/4 px-14 pb-[22px] justify-center border-b-1 border-stroke'>
-        <h3 className='text-2xl font-semibold'>{title}</h3>
-        <CourseSelector courses={fullCourses} onSelectCourse={onSelectCourse} />
-      </div>
-      <div className='px-14'>
-        <AssignmentList courses={courses} selectMode={selectMode} />
-        {!selectMode && (
-          <Button color='tonal' size='compact' content='mixed'>
-            <AddIcon width={12} height={12} />
-            문제 추가
-          </Button>
-        )}
-        <div className='flex justify-end gap-5 mt-3'>
-          <Button color='outlinePurple'>취소</Button>
-          <Button color='primary'>등록</Button>
+    <SurfaceCard size='large' className='mx-auto'>
+      <div className='pt-9 pb-8'>
+        <div className='flex flex-col h-1/4 px-14 pb-[22px] justify-center border-b-1 border-stroke'>
+          <h3 className='text-2xl font-semibold'>{title}</h3>
+          <CourseSelector
+            courses={fullCourses}
+            onSelectCourse={onSelectCourse}
+          />
+        </div>
+        <div className='px-14'>
+          <AssignmentList courses={courses} selectMode={selectMode} />
+          {!selectMode && (
+            <Button color='tonal' size='compact' content='mixed'>
+              <AddIcon width={12} height={12} />
+              문제 추가
+            </Button>
+          )}
+          <div className='flex justify-end gap-5 mt-3'>
+            <Button color='outlinePurple'>취소</Button>
+            <Button color='primary'>등록</Button>
+          </div>
         </div>
       </div>
-    </div>
+    </SurfaceCard>
   );
 };
 
