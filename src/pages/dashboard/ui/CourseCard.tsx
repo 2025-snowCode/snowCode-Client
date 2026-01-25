352d23000d73e37deb612c37bbf929f79f359e19
suminb99 <suminbaek99@gmail.com>
2026-01-25 14:16:36 +0900

    #29 feat: 공통 Dropdown을 적용한 CourseManagementMenu 컴포넌트 구현

--- src/pages/dashboard/ui/CourseCard.tsx
+++ src/pages/dashboard/ui/CourseCard.tsx
@@ -1,13 +1,13 @@
-import {useContext, useState} from 'react';
-import EllipsisIcon from '@/assets/svg/ellipsisIcon.svg?react';
+import {useContext} from 'react';
 import {formateCourseTermWithSlash} from '@/utils/course';
 import {UserTypeContext} from '@/App';
 import type {DashboardCourse} from '@/models/course';
-import {Link} from 'react-router-dom';
+import CourseManagementMenu from './CourseManagementMenu';
 
 const CourseCard = (course: DashboardCourse) => {
   const userType = useContext(UserTypeContext);
   const {
+    id,
     year,
     semester,
     section,
@@ -18,55 +18,33 @@ const CourseCard = (course: DashboardCourse) => {
   } = course;
 
   return (
-    <Link to={`courses/${course.id}`}>
-      <li className='w-148 flex items-stretch bg-white rounded-3xl shadow-card cursor-pointer'>
-        {/* 좌측: 강의 기본 정보 */}
-        <div className='flex flex-col gap-1.5 pl-8 pr-6.5 py-5.5'>
-          <p className='text-sm font-light text-light-black'>
-            {formateCourseTermWithSlash(year, semester, section)}
-          </p>
-          <h3 className='text-[22px]'>{title}</h3>
-          <p className='text-base font-light text-secondary-black line-clamp-3'>
-            {description}
-          </p>
+    <li className='w-148 flex items-stretch bg-white rounded-3xl shadow-card cursor-pointer'>
+      {/* 좌측: 강의 기본 정보 */}
+      <div className='flex flex-col gap-1.5 pl-8 pr-6.5 py-5.5'>
+        <p className='text-sm font-light text-light-black'>
+          {formateCourseTermWithSlash(year, semester, section)}
+        </p>
+        <h3 className='text-[22px]'>{title}</h3>
+        <p className='text-base font-light text-secondary-black line-clamp-3'>
+          {description}
+        </p>
+      </div>
+
+      {/* 우측: 단원 수, 문제 수 */}
+      <div className='relative bg-gray flex-center px-7.5 py-6 rounded-r-3xl text-center text-base font-normal whitespace-nowrap'>
+        <div className='pr-4 border-r-[0.5px] border-[#7A768C]'>
+          <p>단원 수</p>
+          <p className='font-medium'>{unitCount}개</p>
+        </div>
+        <div className='pl-4 border-l-[0.5px] border-[#7A768C]'>
+          <p>문제 수</p>
+          <p className='font-medium'>{assignmentCount}개</p>
         </div>
 
-        {/* 우측: 단원 수, 문제 수 */}
-        <div className='relative bg-[#F9F9F9] flex-center px-7.5 py-6 rounded-r-3xl text-center text-base font-normal whitespace-nowrap'>
-          <div className='pr-4 border-r-[0.5px] border-[#7A768C]'>
-            <p>단원 수</p>
-            <p className='font-medium'>{unitCount}개</p>
-          </div>
-          <div className='pl-4 border-l-[0.5px] border-[#7A768C]'>
-            <p>문제 수</p>
-            <p className='font-medium'>{assignmentCount}개</p>
-          </div>
-
-          {/* 관리자용 강의 관리 버튼 */}
-          {userType === 'admin' && <CourseManageButton />}
-        </div>
-      </li>
-    </Link>
-  );
-};
-
-const CourseManageButton = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
-
-  return (
-    <>
-      <button
-        className='cursor-pointer absolute right-5.5 top-6.5 p-2'
-        onClick={toggleMenu}>
-        <EllipsisIcon className='w-[21.2px] h-[5px]' />
-      </button>
-
-      {menuVisible && <></>}
-    </>
+        {/* 관리자용 강의 관리 버튼 */}
+        {userType === 'admin' && <CourseManagementMenu courseId={id} />}
+      </div>
+    </li>
   );
 };
 
