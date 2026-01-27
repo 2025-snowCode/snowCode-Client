b36bf1d61419ad670245692e4cbfee987cf9c754
suminb99 <suminbaek99@gmail.com>
2026-01-27 18:51:08 +0900

    #17 feat: 문제 선택 - 강의 필터링 로직 구현

--- src/pages/assignment-select/ProblemSelectPage.tsx
+++ src/pages/assignment-select/ProblemSelectPage.tsx
@@ -6,14 +6,50 @@ import {response} from './models/Response';
 import ProblemListContainer from './ui/ProblemListContainer';
 import ActionButtonGroup from './ui/ActionButtonGroup';
 import {formatCourseTermWithSlash} from '@/utils/course';
+import {useEffect, useState} from 'react';
 
 const ProblemSelectPage = () => {
-  const {count, courses} = response.response;
+  const {courses} = response.response;
+  const [selectedCourseId, setSelectedCourseId] = useState<number | null>(null); // null이면 전체 강의 선택
+  const [courseList, setCourseList] = useState(courses); // 표시할 강의 목록
 
-  const COURSE_OPTIONS = courses.map(
-    (course) =>
-      `${course.title} ${formatCourseTermWithSlash(course.year, course.semester, course.section)}`
-  );
+  const handleCourseSelect = (value: string) => {
+    if (value === '전체 강의') {
+      setSelectedCourseId(null);
+      return;
+    }
+
+    const courseId =
+      courses.find(
+        (course) =>
+          `${course.title} ${formatCourseTermWithSlash(
+            course.year,
+            course.semester,
+            course.section
+          )}` === value
+      )?.id || null;
+
+    setSelectedCourseId(courseId);
+  };
+
+  useEffect(() => {
+    if (!selectedCourseId) {
+      setCourseList(courses);
+      return;
+    }
+
+    // 선택된 강의에 해당하는 문제들만 필터링
+    setCourseList(courses.filter((course) => course.id === selectedCourseId));
+  }, [selectedCourseId]);
+
+  // 강의 선택 옵션 생성
+  const COURSE_OPTIONS = [
+    '전체 강의',
+    ...courses.map(
+      (course) =>
+        `${course.title} ${formatCourseTermWithSlash(course.year, course.semester, course.section)}`
+    ),
+  ];
 
   return (
     <SurfaceCard size='large' className='mx-auto'>
@@ -21,13 +57,15 @@ const ProblemSelectPage = () => {
         header={
           <>
             <SectionTitle title='문제 선택' />
-            <CourseSelector options={COURSE_OPTIONS} />
+            <CourseSelector
+              options={COURSE_OPTIONS}
+              onSelect={handleCourseSelect}
+            />
           </>
         }
         list={
           <>
-            <h3 className='text-lg/[27px] font-medium mb-4'>{count}문제</h3>
-            <ProblemListContainer courseList={courses} />
+            <ProblemListContainer courseList={courseList} />
           </>
         }
         footer={<ActionButtonGroup />}
--- src/pages/assignment-select/ui/CourseSelector.tsx
+++ src/pages/assignment-select/ui/CourseSelector.tsx
@@ -1,10 +1,17 @@
 import LabeledDropdown from '@/components/admin/form/LabeledDropdown';
 
-const CourseSelector = ({options}: {options: string[]}) => {
+const CourseSelector = ({
+  options,
+  onSelect,
+}: {
+  options: string[];
+  onSelect: (value: string) => void;
+}) => {
   return (
     <div className='w-81'>
       <LabeledDropdown
         options={options}
+        onSelect={onSelect}
         placeholder='강의를 선택하세요.'
         className='truncate'
       />
