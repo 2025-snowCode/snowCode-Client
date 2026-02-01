707ea69dc0e3464b8616795119a1ea810f105375
suminb99 <suminbaek99@gmail.com>
2026-02-01 20:00:29 +0900

    #17 feat: AssignmentListContainer 타입 안정성 및 이벤트 처리 개선

--- src/pages/assignment-select/ui/AssignmentListContainer.tsx
+++ src/pages/assignment-select/ui/AssignmentListContainer.tsx
@@ -1,3 +1,7 @@
+import type {AssignmentSelectCourse} from '@/models/course';
+
+type T = AssignmentSelectCourse['assignments'][number];
+
 interface AssignmentListContainerProps<T> {
   items: T[];
   renderItem: (item: T) => React.ReactNode;
@@ -11,12 +15,16 @@ const AssignmentListContainer = ({
   renderItem,
   title,
 }: AssignmentListContainerProps<T>) => {
+  const handleSelect = (id: number, event: React.MouseEvent) => {
+    event.stopPropagation();
+    onSelect(id);
+  };
   return (
     <div>
       <h3 className='text-lg/[27px] font-medium mb-4'>{title}</h3>
       <ul className='w-full flex flex-col pr-6 h-96 pb-5 overflow-y-auto gap-2.5'>
         {items.map((item) => (
-          <li onClick={() => onSelect(item.id)} key={item.id}>
+          <li onClick={(e) => handleSelect(item.id, e)} key={item.id}>
             {renderItem(item)}
           </li>
         ))}
