1c762a2c2c377d3362a0086832cbecc3dc943d66
suminb99 <suminbaek99@gmail.com>
2026-02-02 16:54:16 +0900

    #17 chore: 불필요한 파일 삭제

--- src/pages/assignment-select/ui/SectionTitle.tsx
+++
@@ -1,7 +0,0 @@
-const SectionTitle = ({title}: {title: string}) => {
-  return (
-    <h2 className='text-primary-black text-2xl/9 font-semibold'>{title}</h2>
-  );
-};
-
-export default SectionTitle;
