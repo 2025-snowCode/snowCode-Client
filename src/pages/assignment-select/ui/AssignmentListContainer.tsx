fa31216230cdc3625414a66fa93d0be8ff904b55
suminb99 <suminbaek99@gmail.com>
2026-02-02 16:28:02 +0900

    #17 style: 컴스텀 스크롤바 구현 및 적용

--- src/pages/assignment-select/ui/AssignmentListContainer.tsx
+++ src/pages/assignment-select/ui/AssignmentListContainer.tsx
@@ -20,9 +20,9 @@ const AssignmentListContainer = ({
     onSelect(id);
   };
   return (
-    <div>
+    <div className='custom-scrollbar'>
       <h3 className='text-lg/[27px] font-medium mb-4'>{title}</h3>
-      <ul className='w-full flex flex-col pr-6 h-96 pb-5 overflow-y-auto gap-2.5'>
+      <ul className='w-full flex flex-col pr-4 h-96 pb-5 overflow-y-auto gap-2.5'>
         {items.map((item) => (
           <li onClick={(e) => handleSelect(item.id, e)} key={item.id}>
             {renderItem(item)}
