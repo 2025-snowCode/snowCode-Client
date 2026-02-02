e9eff118c4a45c4311f4edcafc2630069a5618f7
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-03 01:38:44 +0900

    #31 feat: pagination 공통 컴포넌트 구현

---
+++ src/shared/ui/pagination/pagination.tsx
@@ -0,0 +1,47 @@
+import ChevronDown from '@assets/icons/chevron-down.svg?react';
+
+export default function Pagination({
+  totalItems,
+  pageSize,
+  currentPage,
+  onPageChange,
+}: {
+  totalItems: number;
+  pageSize: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}) {
+  const totalPages = Math.ceil(totalItems / pageSize);
+
+  if (totalPages == 0) {
+    return null;
+  }
+
+  return (
+    <div className='flex items-center gap-6'>
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}>
+        <ChevronDown className='rotate-90 w-2 h-4' />
+      </button>
+
+      {Array.from({length: totalPages}, (_, i) => i + 1).map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={
+            currentPage === page
+              ? 'text-primary'
+              : 'text-black-light hover:text-primary transition-colors'
+          }>
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}>
+        <ChevronDown className='-rotate-90 w-2 h-4' />
+      </button>
+    </div>
+  );
+}
