774ae28b70ee3ffff94dad68497a1eb9837aa8a2
suminb99 <suminbaek99@gmail.com>
2026-02-03 16:20:06 +0900

    #34 feat: axios 인스턴스 및 인증 interceptor 설정

---
+++ src/shared/api/axiosInstance.ts
@@ -0,0 +1,37 @@
+import axios, {type AxiosInstance} from 'axios';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+/**
+ * publicAxios: 인증 불필요한 요청용
+ * 예: 로그인, 회원가입
+ */
+export const publicAxios: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+/**
+ * privateAxios: 인증 필요한 요청용
+ * 예: 대시보드, 강의상세정보, 학생관리 등
+ */
+export const privateAxios: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+privateAxios.interceptors.request.use(
+  (config) => {
+    const accessToken = import.meta.env.VITE_DEV_ACCESS_TOKEN;
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
