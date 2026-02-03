9db1b0a8a3986ba5a7638adac0fe4fd703cebb76
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-03 20:49:15 +0900

    #31 feat: 학생 관리 페이지 구현

--- src/pages/admin/student/studentManagementPage.tsx
+++ src/pages/admin/student/studentManagementPage.tsx
@@ -1,27 +1,27 @@
 import AssignmentFormLayout from '@/components/admin/assignments/AssignmentFormLayout';
 import Search from '@/assets/svg/search.svg?react';
-import {useSearchParams} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
 import Pagination from '@/shared/ui/pagination/pagination';
 import {useState} from 'react';
 import Input from '@/components/common/Input';
+import StudentTable from '@/entities/student/studentTable';
+import mockCourseStudents from '@/entities/student/model/mock';
 
 export default function StudentManagementPage() {
-  const [searchParams] = useSearchParams();
   const [currentPage, setCurrentPage] = useState(1);
-  const course = searchParams.get('course');
   const {register} = useForm();
+
+  const { response } = mockCourseStudents;
 
   const titleExtra = (
     <div className='flex justify-between items-center flex-1'>
       <button className=' text-primary px-3 py-[6px] rounded-4xl inline-flex h-8 justify-center items-center border border-primary'>
-        {course || '전체'}
+        {response.title} ({response.section})
       </button>
       <Input
         type='text'
         placeholder='학번을 검색하세요'
         icon={<Search className='w-4 h-4' />}
-        className='w-64'
         {...register('studentSearch')}
       />
     </div>
@@ -32,9 +32,11 @@ export default function StudentManagementPage() {
       <AssignmentFormLayout
         title='학생 관리'
         titleExtra={titleExtra}
-        content={<div>학생 관리 기능이 여기에 들어갑니다.</div>}
+        content={<StudentTable students={response.students} />}
         onCancel={() => {}}
         onConfirm={() => {}}
+        cancelLabel='삭제'
+        confirmLabel='등록'
       />
       <Pagination
         totalItems={100}
