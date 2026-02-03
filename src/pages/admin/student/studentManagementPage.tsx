d2dcecb3fb8d64435468dcc8ea4e3de0e84b8531
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-03 13:44:27 +0900

    #31 feat: 학생 관리 페이지 기능 추가(검색 입력 + 페이지네이션)

--- src/pages/admin/student/studentManagementPage.tsx
+++ src/pages/admin/student/studentManagementPage.tsx
@@ -1,34 +1,49 @@
 import AssignmentFormLayout from '@/components/admin/assignments/AssignmentFormLayout';
 import Search from '@/assets/svg/search.svg?react';
 import {useSearchParams} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+import Pagination from '@/shared/ui/pagination/pagination';
+import {useState} from 'react';
+import Input from '@/components/common/Input';
 
 export default function StudentManagementPage() {
   const [searchParams] = useSearchParams();
+  const [currentPage, setCurrentPage] = useState(1);
   const course = searchParams.get('course');
+  const {register} = useForm();
 
   const titleExtra = (
     <div className='flex justify-between items-center flex-1'>
       <button className=' text-primary px-3 py-[6px] rounded-4xl inline-flex h-8 justify-center items-center border border-primary'>
         {course || '전체'}
       </button>
-      <div className='relative'>
-        <input
-          type='text'
-          placeholder='학번을 검색하세요'
-          className='border border-gray-300 rounded-lg px-4 py-2 pr-10 w-64'
-        />
-        <Search className='absolute right-3 top-3 w-4 h-4 text-gray-400' />
-      </div>
+      <Input
+        type='text'
+        placeholder='학번을 검색하세요'
+        icon={<Search className='w-4 h-4' />}
+        className='w-64'
+        {...register('studentSearch')}
+      />
     </div>
   );
 
   return (
-    <AssignmentFormLayout
-      title='학생 관리'
-      titleExtra={titleExtra}
-      content={<div>학생 관리 기능이 여기에 들어갑니다.</div>}
-      onCancel={() => {}}
-      onConfirm={() => {}}
-    />
+    <div className='flex flex-col items-center gap-6'>
+      <AssignmentFormLayout
+        title='학생 관리'
+        titleExtra={titleExtra}
+        content={<div>학생 관리 기능이 여기에 들어갑니다.</div>}
+        onCancel={() => {}}
+        onConfirm={() => {}}
+      />
+      <Pagination
+        totalItems={100}
+        pageSize={10}
+        currentPage={currentPage}
+        onPageChange={(page) => {
+          setCurrentPage(page);
+        }}
+      />
+    </div>
   );
 }
