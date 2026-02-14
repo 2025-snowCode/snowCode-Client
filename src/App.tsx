66f1b421a32c54404e3dcfcbc7132729c70ff928
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-02-14 17:38:07 +0900

    #41 feat: 라우팅 시스템에 카카오 인증 통합

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import AssignmentCreatePage from './pages/admin/assignments/AssignmentCreatePage
 import CourseCreatePage from './pages/admin/courses/CourseCreatePage';
 import StudentManagementPage from './pages/admin/student/StudentManagementPage';
 import StudentProfilePage from './pages/admin/student/StudentProfilePage';
+import KakaoCallbackPage from './pages/common/KakaoCallbackPage';
+import PrivateRoute from '@/shared/ui/PrivateRoute';
 import {useEffect} from 'react';
 import {useUserStore} from '@/entities/auth/model/useUserStore';
 
@@ -31,26 +33,30 @@ const AppRoutes = () => {
         {/* 공통 영역 */}
         <Route index element={<LandingPage />} />
         <Route path='userid' element={<UserIdInputPage />} />
+        <Route path='auth/kakao/callback' element={<KakaoCallbackPage />} />
 
-        {/* 학생 영역 */}
-        <Route path='student'>
-          <Route index element={<Dashboard />} />
-          <Route path='courses/:id' element={<CourseOverviewPage />} />
-        </Route>
+        {/* 인증 필요 영역 */}
+        <Route element={<PrivateRoute />}>
+          {/* 학생 영역 */}
+          <Route path='student'>
+            <Route index element={<Dashboard />} />
+            <Route path='courses/:id' element={<CourseOverviewPage />} />
+          </Route>
 
-        {/* 관리자 영역 */}
-        <Route path='admin'>
-          <Route index element={<Dashboard />} />
-          {/* <Route path='assignments' element={<AssignmentsPage />} /> */}
-          <Route path='assignments/create' element={<AssignmentCreatePage />} />
-          <Route path='assignments/:id' element={<AssignmentSelectPage />} />
-          <Route path='courses/:id' element={<CourseOverviewPage />} />
-          <Route path='courses/create' element={<CourseCreatePage />} />
-          <Route path='student' element={<StudentManagementPage />} />
-          <Route
-            path='student/profile/:studentId'
-            element={<StudentProfilePage />}
-          />
+          {/* 관리자 영역 */}
+          <Route path='admin'>
+            <Route index element={<Dashboard />} />
+            {/* <Route path='assignments' element={<AssignmentsPage />} /> */}
+            <Route path='assignments/create' element={<AssignmentCreatePage />} />
+            <Route path='assignments/:id' element={<AssignmentSelectPage />} />
+            <Route path='courses/:id' element={<CourseOverviewPage />} />
+            <Route path='courses/create' element={<CourseCreatePage />} />
+            <Route path='student' element={<StudentManagementPage />} />
+            <Route
+              path='student/profile/:studentId'
+              element={<StudentProfilePage />}
+            />
+          </Route>
         </Route>
       </Route>
     </Routes>
