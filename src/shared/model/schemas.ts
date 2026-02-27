69590375aec69d3a41c2f554cd3d35baefe09f20
suminb99 <suminbaek99@gmail.com>
2026-02-28 02:09:23 +0900

    #62 feat: errorResponseSchema 추가

--- src/shared/model/schemas.ts
+++ src/shared/model/schemas.ts
@@ -3,6 +3,18 @@ import {z} from 'zod';
 export const apiResponseSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
   z.object({success: z.boolean(), response: dataSchema});
 
+export const errorResponseSchema = apiResponseSchema(
+  z.object({
+    errorCode: z.string(),
+    errorMessage: z.string(),
+    errors: z.object({
+      additionalProp1: z.string(),
+      additionalProp2: z.string(),
+      additionalProp3: z.string(),
+    }),
+  })
+);
+
 export const semesterCodeSchema = z.enum([
   'FIRST',
   'SECOND',
