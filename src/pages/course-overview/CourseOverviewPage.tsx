308f316b9e6807569d40d60b08343319d99ad706
suminb99 <suminbaek99@gmail.com>
2026-02-28 22:15:51 +0900

    #59 feat: 강의상세페이지 api 연동

--- src/pages/course-overview/CourseOverviewPage.tsx
+++ src/pages/course-overview/CourseOverviewPage.tsx
@@ -1,21 +1,27 @@
 import {getTotalAssignmentCount} from '@/shared/lib/course';
 import CourseContent from './ui/CourseContent';
 import CourseHero from './ui/CourseHero';
-import {courseResponse} from '@/shared/mocks/courseOverviewResponse';
+import {useParams} from 'react-router-dom';
+import {useSuspenseQuery} from '@tanstack/react-query';
+import {courseQueries} from '@/entities/course/api/courseQueries';
 
 const CourseOverviewPage = () => {
-  const courseData = courseResponse.response;
-  const totalAssignmentCount = getTotalAssignmentCount(courseData.units); // 총 문제 수 계산
-  const hasUnits = courseData.unitCount !== 0 ? true : false;
+  const {id} = useParams();
+  const courseId = Number(id);
+  const {data: course} = useSuspenseQuery(
+    courseQueries.getCourseDetails(courseId)
+  );
+  const totalAssignmentCount = getTotalAssignmentCount(course.units); // 총 문제 수 계산
+  const hasUnits = course.unitCount !== 0 ? true : false;
 
   return (
     <div className='w-full min-h-screen flex flex-col absolute top-0 left-0 z-0 p-4'>
       <CourseHero
-        courseData={courseData}
+        course={course}
         assignmentCount={totalAssignmentCount}
         isActiveCourse={hasUnits}
       />
-      <CourseContent units={courseData.units} isActiveCourse={hasUnits} />
+      <CourseContent units={course.units} isActiveCourse={hasUnits} />
     </div>
   );
 };
