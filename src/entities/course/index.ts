c87a6e29a55786d6dac26ee97319ca0ed87a8b80
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-03-05 13:15:34 +0900

    #53 refactor: 기존 강의 생성 로직 제거 및 entity exports 정리

--- src/entities/course/index.ts
+++ src/entities/course/index.ts
@@ -1,2 +1,4 @@
 export {getAllCourses, deleteCourse, createCourse} from './api/courseApi';
-export {useCreateCourseMutation} from './api/useCreateCourseMutation';
+export {default as courseQueryOptions} from './api/courseQueryOptions';
+export {createCourseRequestSchema, createCourseResponseSchema} from './model/courseSchema';
+export type {CreateCourseRequest, CreateCourseResponse} from './model/courseSchema';
