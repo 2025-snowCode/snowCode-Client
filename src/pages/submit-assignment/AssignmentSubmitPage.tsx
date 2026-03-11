008b475a022e9b39288c077195ebc4a08ec9a42e
suminb99 <suminbaek99@gmail.com>
2026-03-11 23:09:36 +0900

    #65 style: layout 수정

--- src/pages/submit-assignment/AssignmentSubmitPage.tsx
+++ src/pages/submit-assignment/AssignmentSubmitPage.tsx
@@ -1,5 +1,6 @@
 import SurfaceCard from '@/shared/ui/SurfaceCard';
 import AssignmentDetails from './ui/AssignmentDetails';
+import CodeEditor from './ui/CodeEditor';
 
 const AssignmentSubmitPage = () => {
   return (
@@ -7,8 +8,10 @@ const AssignmentSubmitPage = () => {
       <SurfaceCard className='w-133.5 min-w-0'>
         <AssignmentDetails />
       </SurfaceCard>
-      <div className='flex-1 bg-primary-black rounded-[30px]'>
+
+      <div className='flex-1 min-h-190 rounded-[30px]'>
         {/* TODO: 코드 에디터 및 제출상태 표시 영역*/}
+        <CodeEditor />
       </div>
     </div>
   );
