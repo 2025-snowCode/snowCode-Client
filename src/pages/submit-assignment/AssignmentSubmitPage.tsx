7ab16d0a0024602d6cad3bbe954c12cf21a17c73
suminb99 <suminbaek99@gmail.com>
2026-03-16 00:10:13 +0900

    #71 feat: 과제 제출 결과 모달 연동

--- src/pages/submit-assignment/AssignmentSubmitPage.tsx
+++ src/pages/submit-assignment/AssignmentSubmitPage.tsx
@@ -5,6 +5,8 @@ import {assignmentQueries} from '@/entities/assignment/api/assignmentQueries';
 import {useLocation, useParams} from 'react-router-dom';
 import {useSuspenseQueries} from '@tanstack/react-query';
 import {courseQueries} from '@/entities/course/api/courseQueries';
+import {useAssignmentSubmit} from '@/features/assignment/submit-assignment/lib/useAssignmentSubmit';
+import SubmitResultModal from './ui/SubmitResultModal';
 
 const AssignmentSubmitPage = () => {
   const location = useLocation();
@@ -20,17 +22,26 @@ const AssignmentSubmitPage = () => {
     }
   );
 
+  const {onSubmit, isSubmitPending, result, isModalOpen, closeModal} =
+    useAssignmentSubmit(courseDetails, Number(assignmentId));
+
   return (
-    <div className='h-full flex gap-4'>
-      <AssignmentSideBar units={courseDetails.units} />
-      <div className='w-127.5 h-full overflow-hidden custom-scrollbar py-3 px-2 bg-white rounded-[30px] shadow-card'>
-        <AssignmentDetails index={index} {...assignmentDetails} />
+    <>
+      <div className='h-full flex gap-4'>
+        <AssignmentSideBar units={courseDetails.units} />
+        <div className='w-127.5 h-full overflow-hidden custom-scrollbar py-3 px-2 bg-white rounded-[30px] shadow-card'>
+          <AssignmentDetails index={index} {...assignmentDetails} />
+        </div>
+        <div className='flex-1 min-w-0'>
+          {/* TODO: 코드 에디터 및 제출상태 표시 영역 */}
+          <CodeEditor onSubmit={onSubmit} isSubmitPending={isSubmitPending} />
+        </div>
+
+        {isModalOpen && result && (
+          <SubmitResultModal result={result} closeModal={closeModal} />
+        )}
       </div>
-      <div className='flex-1 min-w-0'>
-        {/* TODO: 코드 에디터 및 제출상태 표시 영역*/}
-        <CodeEditor />
-      </div>
-    </div>
+    </>
   );
 };
 
