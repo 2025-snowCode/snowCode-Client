f1ac0e4691c93ec135fd48e124bc2f952cd9858a
suminb99 <suminbaek99@gmail.com>
2026-03-16 00:07:37 +0900

    #71 feat: 과제 제출 mutation 추가

--- src/entities/assignment/api/assignmentMutations.ts
+++ src/entities/assignment/api/assignmentMutations.ts
@@ -1,8 +1,21 @@
-import {deleteAssignment} from './assignmentApi';
+import {deleteAssignment, submitAssignment} from './assignmentApi';
 
 export const assignmentMutations = {
   deleteAssignment: {
     mutationKey: ['deleteAssignment'],
     mutationFn: (assignmentId: number) => deleteAssignment(assignmentId),
+  },
+
+  submitAssignment: {
+    mutationKey: ['submitAssignment'],
+    mutationFn: ({
+      unitId,
+      assignmentId,
+      code,
+    }: {
+      unitId: number;
+      assignmentId: number;
+      code: string;
+    }) => submitAssignment(unitId, assignmentId, code),
   },
 };
