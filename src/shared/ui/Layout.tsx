2ebd0b491f196578616fb6177ead58bc9c6afb10
suminb99 <suminbaek99@gmail.com>
2026-03-25 15:39:46 +0900

    #75 feat: 레이아웃에 suspense 경계 적용

--- src/shared/ui/Layout.tsx
+++ src/shared/ui/Layout.tsx
@@ -1,19 +1,23 @@
 import {Outlet} from 'react-router-dom';
 import Header from '@/shared/ui/Header';
+import {Suspense} from 'react';
+import Loading from './Loading';
 
 const Layout = () => {
   return (
-    <div className='min-h-screen overflow-x-hidden flex justify-center'>
-      <div className='min-w-289 px-4 py-12'>
-        <div className='w-289 pb-8 mx-auto relative z-10'>
-          <Header />
-        </div>
+    <Suspense fallback={<Loading />}>
+      <div className='min-h-screen overflow-x-hidden flex justify-center'>
+        <div className='min-w-289 px-4 py-12'>
+          <div className='w-289 pb-8 mx-auto relative z-10'>
+            <Header />
+          </div>
 
-        <main>
-          <Outlet />
-        </main>
+          <main>
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
