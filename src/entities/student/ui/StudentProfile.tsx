4738d553e42f7a04d7f210c1cfcafe992329ef38
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-03-27 00:13:18 +0900

    #92 feat: 학생 프로필에서 1:1 채팅 이동 기능 추가

--- src/entities/student/ui/StudentProfile.tsx
+++ src/entities/student/ui/StudentProfile.tsx
@@ -2,22 +2,31 @@ import ProfileImage from '@/assets/svg/profileImage.svg?react';
 import ChatIcon from '@/assets/svg/chatIcon.svg?react';
 import MailIcon from '@/assets/svg/mailIcon.svg?react';
 import Button from '@/shared/ui/button/Button';
-
 interface StudentProfileProps {
   name: string;
   studentId: string;
+  onChat?: () => void;
 }
 
-export const StudentProfile = ({name, studentId}: StudentProfileProps) => {
+export const StudentProfile = ({
+  name,
+  studentId,
+  onChat,
+}: StudentProfileProps) => {
   return (
-    <div className='flex flex-col bg-white w-[450px] h-[470px] rounded-[30px] items-center justify-center shrink-0 gap-[25.5px]'>
-      <ProfileImage className='w-[180px] h-[180px]' />
+    <div className='flex flex-col bg-white w-112.5 h-117.5 rounded-[30px] items-center justify-center shrink-0 gap-[25.5px]'>
+      <ProfileImage className='w-45 h-45' />
       <div>
         <p className='text-2xl text-black font-semibold'>{name}</p>
         <p className='text-secondary-black text-lg font-normal'>{studentId}</p>
       </div>
-      <div className='flex gap-[22px] pt-[11px]'>
-        <Button color='primary' size='compact' content='mixed' type='button'>
+      <div className='flex gap-5.5 pt-2.75'>
+        <Button
+          color='primary'
+          size='compact'
+          content='mixed'
+          type='button'
+          onClick={onChat}>
           <div
             className='flex gap-2.5 items-center
         '>
