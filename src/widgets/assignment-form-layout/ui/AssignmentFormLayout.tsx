46b65873110c035d264c2320d2d8b4a48076a067
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-03-27 03:17:35 +0900

    #98 style: 문제 등록 및 수정 페이지 너비 수정

--- src/widgets/assignment-form-layout/ui/AssignmentFormLayout.tsx
+++ src/widgets/assignment-form-layout/ui/AssignmentFormLayout.tsx
@@ -26,7 +26,7 @@ const AssignmentFormLayout = ({
     <div className='flex flex-col items-center gap-6'>
       <SurfaceCard
         size='medium'
-        className='mx-auto pt-9 pb-8 px-14 flex flex-col'>
+        className='w-[900px] pt-9 pb-8 px-14 flex flex-col'>
         {/* 제목 */}
         <div className='flex items-center gap-3'>
           <h2 className='text-black text-[24px] font-semibold'>{title}</h2>
@@ -34,7 +34,7 @@ const AssignmentFormLayout = ({
         </div>
 
         {/* 본문 */}
-        <div className='flex-1 mt-7'>{content}</div>
+        <div className='flex-1 mt-7 w-full'>{content}</div>
 
         {/* 하단 버튼 */}
         <div className='mt-7 flex justify-end gap-[21px]'>
